ecdd9baaef486da9987cce03edf4923b88981f7f
PakoPortalo <pakoportalo@gmail.com>
2020-09-01 12:40:38 +0200

    Test d and precition --> 5

--- ft_putnbrMaster.c
+++ ft_putnbrMaster.c
@@ -6,7 +6,7 @@
 /*   By: pako <pako@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/17 10:40:44 by pako              #+#    #+#             */
-/*   Updated: 2020/09/01 12:37:54 by pako             ###   ########.fr       */
+/*   Updated: 2020/09/01 12:40:08 by pako             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -91,7 +91,7 @@ int		ft_nbrZero(int n, int digit)
 	{
 		ret += write(1, "-", 1);
 		n = n * -1;
-		if (digit != n)
+		if (digit < n)
 			digit++;
 	}
 	else if (n == 0)
