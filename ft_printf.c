96d904b5292f23a88f84b06e1d51f2fa2e1b020f
PakoPortalo <pakoportalo@gmail.com>
2020-09-23 19:31:19 +0200

    Setting putstrMaster 6

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: pako <pako@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/28 17:37:43 by pako              #+#    #+#             */
-/*   Updated: 2020/09/23 19:29:10 by pako             ###   ########.fr       */
+/*   Updated: 2020/09/23 19:30:58 by pako             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,7 @@ int ft_printf(const char *format, ...)
 {
 	va_list ap;
 	t_flags data;
-	int ret;
-	int i;
 
-	i = 0;
-	ret = 0;
 	data = ft_resetflags();
 	va_start(ap, format);
 	while (format[data.i] != '\0')
@@ -39,10 +35,6 @@ int ft_printf(const char *format, ...)
 			data = ft_varChannel(format, ap, data);
 		}
 		data.i++;
-		i = data.i;
-		ret += data.ret;
-		data = ft_resetflags();
-		data.i = i;
 	}
 	return (data.ret);
 }
