7a20b1aaf84661c3ad92250a338ea7e00a37e7e9
PakoPortalo <pakoportalo@gmail.com>
2020-09-24 18:36:46 +0200

    Setting Hexadecimal 3

--- ft_hexmasterUtils1.c
+++ ft_hexmasterUtils1.c
@@ -6,7 +6,7 @@
 /*   By: pako <pako@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/24 13:07:44 by pako              #+#    #+#             */
-/*   Updated: 2020/09/24 18:10:00 by pako             ###   ########.fr       */
+/*   Updated: 2020/09/24 18:34:12 by pako             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,25 +31,24 @@ unsigned int		ft_hexDigit(unsigned int n, t_flags data)
 	return (data.digit);
 }
 
-int		ft_puthex(unsigned int n, t_flags data)
+void		ft_puthex(unsigned int n, t_flags *data)
 {
-	char		c;
-	unsigned int h;
+	char			c;
+	unsigned int	h;
 
 	if (n > 0)
 	{
-		data.ret = ft_puthex(n / 16, data);
+		ft_puthex(n / 16, data);
 		h = n % 16;
 		if (h > 9)
-			if (data.upperHex == 1)
+			if ((*data).upperHex == 1)
 				c = (h - 10) + 'A';
 			else
 				c = (h - 10) + 'a';
 		else
 			c = h + '0';
-		data.ret = write(1, &c, 1);
+		(*data).ret += write(1, &c, 1);
 	}
-	return (data.ret);
 }
 
 t_flags		ft_subhexprecition2(t_flags data)
