fc8b47fab73bcaddaa3e72fb6d50d01cdbfe7dff
PakoPortalo <pakoportalo@gmail.com>
2020-09-25 13:17:49 +0200

    Setting Pointers 23

--- ft_putptrMaster.c
+++ ft_putptrMaster.c
@@ -6,7 +6,7 @@
 /*   By: pako <pako@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/24 19:29:14 by pako              #+#    #+#             */
-/*   Updated: 2020/09/25 13:16:43 by pako             ###   ########.fr       */
+/*   Updated: 2020/09/25 13:17:43 by pako             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,7 +64,7 @@ t_flags	ft_putptrMaster2(unsigned long n, t_flags data)
 t_flags	ft_putptrMaster(unsigned long n, t_flags data)
 {
 	data.uns = n;
-	if ((n == NULL) || (data.width >= 2))
+	if ((n == NULL) && (data.width >= 2))
 		data.width -= 2;
 	data.digit = ft_ptrDigit(n, data);
 	if (data.minus == 0)
