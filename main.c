2f1456a400d633320995482332f8026ead785384
PakoPortalo <pakoportalo@gmail.com>
2020-09-25 13:15:11 +0200

    Setting Pointers 22

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: pako <pako@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/18 10:07:27 by pako              #+#    #+#             */
-/*   Updated: 2020/09/25 13:04:25 by pako             ###   ########.fr       */
+/*   Updated: 2020/09/25 13:12:10 by pako             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -294,16 +294,16 @@ int		main()
 	//printf("printf output:    |%5.p|\n", NULL);
 
 
-	//ft_printf("ft_printf output: |%2.9p\n|", 1234);
-	//printf("printf output:    |%2.9p\n|", 1234);
-	//ft_printf("ft_printf output: |%9.2p\n|", 1234);
-	//printf("printf output:    |%9.2p\n|", 1234);
-	//ft_printf("ft_printf output: |%p|\n", 4);
-	//printf("printf output:    |%p|\n", 4);
-	//ft_printf("ft_printf output: |%5p|\n", 14);
-	//printf("printf output:    |%5p|\n", 14);
-	//ft_printf("ft_printf output: |%2p|\n", 3);
-	//printf("printf output:    |%2p|\n", 3);
+	ft_printf("ft_printf output: |%2.9p\n|", 1234);
+	printf("printf output:    |%2.9p\n|", 1234);
+	ft_printf("ft_printf output: |%9.2p\n|", 1234);
+	printf("printf output:    |%9.2p\n|", 1234);
+	ft_printf("ft_printf output: |%p|\n", 4);
+	printf("printf output:    |%p|\n", 4);
+	ft_printf("ft_printf output: |%5p|\n", 14);
+	printf("printf output:    |%5p|\n", 14);
+	ft_printf("ft_printf output: |%2p|\n", 3);
+	printf("printf output:    |%2p|\n", 3);
 	ft_printf("ft_printf output: |%70p|\n", &a);
 	printf("printf output:    |%70p|\n", &a);
 
