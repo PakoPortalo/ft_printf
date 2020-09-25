a1f875ab40a2f8787fef41119a0bf5b4b8d145b6
PakoPortalo <pakoportalo@gmail.com>
2020-09-25 14:18:35 +0200

    Setting Pointers 33

--- ft_ptrmasterUtils2.c
+++ ft_ptrmasterUtils2.c
@@ -6,7 +6,7 @@
 /*   By: pako <pako@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/24 19:30:26 by pako              #+#    #+#             */
-/*   Updated: 2020/09/25 14:03:51 by pako             ###   ########.fr       */
+/*   Updated: 2020/09/25 14:18:15 by pako             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 t_flags		ft_ptrprinter1(unsigned long n, t_flags data)
 {
-		if ((data.width > data.precition) && (n != NULL))
+		if (((data.width > data.precition) && !(data.precition < data.digit)) && (n != NULL))
 				data.width -= 2;
 		while ((data.width - data.digit) != 0)
 		{
