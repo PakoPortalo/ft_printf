28cfd736c2c5ef18a29d55659306907dfd5d0375
PakoPortalo <pakoportalo@gmail.com>
2020-09-26 20:02:59 +0200

    Cleaning Printf 6

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: pako <pako@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/28 17:37:43 by pako              #+#    #+#             */
-/*   Updated: 2020/09/26 19:29:11 by pako             ###   ########.fr       */
+/*   Updated: 2020/09/26 20:02:35 by pako             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,10 @@ int ft_printf(const char *format, ...)
 			if(ft_strchr("%cspdiuxX", format[data.i]))
 				data = ft_varChannel(format, ap, data);
 			else
+			{
 				data.ret += write(1, &format[data.i], 1);
+				data = ft_keepresetflags(data);
+			}
 
 		}
 		data.i++;
