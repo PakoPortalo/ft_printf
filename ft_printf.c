2ed54d757ade9f00e427058bf9c5f037fad5a9a7
PakoPortalo <pakoportalo@gmail.com>
2020-09-27 17:58:42 +0200

    solving %-5 1

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: pako <pako@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/28 17:37:43 by pako              #+#    #+#             */
-/*   Updated: 2020/09/27 17:53:38 by pako             ###   ########.fr       */
+/*   Updated: 2020/09/27 17:56:43 by pako             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,8 +40,9 @@ int ft_printf(const char *format, ...)
 			//		data.ret += write(1, &format[data.i], 1);
 			//}
 		}
-		if (format[data.i + 1] != '\0')
+		if (format[data.i] != '\0')
 			data.i++;
 	}
+	va_end(ap);
 	return (data.ret);
 }
