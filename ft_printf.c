901792f4d29366519c829973e7811384c5b7d2d9
PakoPortalo <pakoportalo@gmail.com>
2020-09-27 17:42:38 +0200

    Norminetting 6

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: pako <pako@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/28 17:37:43 by pako              #+#    #+#             */
-/*   Updated: 2020/09/27 17:39:34 by pako             ###   ########.fr       */
+/*   Updated: 2020/09/27 17:42:24 by pako             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ int ft_printf(const char *format, ...)
 				data = ft_varchannel(format, ap, data);
 			else
 			{
-				if (format[data.i + 1] != '\0')
+				if (format[data.i] != '\0')
 					data.ret += write(1, &format[data.i], 1);
 			}
 		}
