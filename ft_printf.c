c35c578438d4bd64d705c2bc5ac9765a575ea9f8
PakoPortalo <pakoportalo@gmail.com>
2020-09-27 18:31:53 +0200

    Tristesa es lo que siento (%-5)

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: pako <pako@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/28 17:37:43 by pako              #+#    #+#             */
-/*   Updated: 2020/09/27 18:16:48 by pako             ###   ########.fr       */
+/*   Updated: 2020/09/27 18:31:25 by pako             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,11 +29,8 @@ int ft_printf(const char *format, ...)
 			data.i++;
 			while (ft_strchr("-0.*123456789", format[data.i]))
 			{
-				if (format[data.i] != '\0')
-				{
 					data = ft_flags(data, format, ap);
 					data.i++;
-				}
 			}
 			if(ft_strchr("%cspdiuxX", format[data.i]))
 				data = ft_varchannel(format, ap, data);
