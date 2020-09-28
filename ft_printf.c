0ca7e21cb0db2a7c3855cf7663a8bcaa4f42e2ac
PakoPortalo <pakoportalo@gmail.com>
2020-09-28 10:01:24 +0200

    last problems 4

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: pako <pako@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/28 17:37:43 by pako              #+#    #+#             */
-/*   Updated: 2020/09/28 09:51:53 by pako             ###   ########.fr       */
+/*   Updated: 2020/09/28 10:00:44 by pako             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,13 @@ t_flags		ft_printf1(const char *format, t_flags data, va_list ap)
 			data.i++;
 		else
 			break ;
+	}
+	if (ft_strchr("%cspdiuxX", format[data.i]))
+		data = ft_varchannel(format, ap, data);
+	else
+	{
+		if (format[data.i])
+			data.ret += write(1, &format[data.i], 1);
 	}
 	return (data);
 }
@@ -40,15 +47,7 @@ int			ft_printf(const char *format, ...)
 		{
 			data = ft_keepresetflags(data);
 			data.i++;
-			while (ft_strchr("-0.*123456789", format[data.i]))
-				data = ft_printf1(format, data, ap);
-			if (ft_strchr("%cspdiuxX", format[data.i]))
-				data = ft_varchannel(format, ap, data);
-			else
-			{
-				if (format[data.i])
-					data.ret += write(1, &format[data.i], 1);
-			}
+			data = ft_printf1(format, data, ap);
 		}
 		if (format[data.i] != '\0')
 			data.i++;
