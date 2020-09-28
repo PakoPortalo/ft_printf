f94c75f9a6ebdf707d5680224016c846f5a4afeb
PakoPortalo <pakoportalo@gmail.com>
2020-09-28 09:30:04 +0200

    solving %-5 3

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: pako <pako@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/28 17:37:43 by pako              #+#    #+#             */
-/*   Updated: 2020/09/27 18:31:25 by pako             ###   ########.fr       */
+/*   Updated: 2020/09/28 09:29:01 by pako             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,13 +30,16 @@ int ft_printf(const char *format, ...)
 			while (ft_strchr("-0.*123456789", format[data.i]))
 			{
 					data = ft_flags(data, format, ap);
-					data.i++;
+					if (format[data.i] != '\0')
+						data.i++;
+					else
+						break;
 			}
 			if(ft_strchr("%cspdiuxX", format[data.i]))
 				data = ft_varchannel(format, ap, data);
 			else
 			{
-				if (format[data.i] != '\0')
+				if (format[data.i])
 					data.ret += write(1, &format[data.i], 1);
 			}
 		}
