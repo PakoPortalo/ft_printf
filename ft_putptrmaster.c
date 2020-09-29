022ab7ca910fe72bed0941575bdc617d3646e581
PakoPortalo <pakoportalo@gmail.com>
2020-09-29 19:14:59 +0200

    width + precition 10

--- ft_putptrmaster.c
+++ ft_putptrmaster.c
@@ -6,7 +6,7 @@
 /*   By: pako <pako@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/24 19:29:14 by pako              #+#    #+#             */
-/*   Updated: 2020/09/29 19:12:08 by pako             ###   ########.fr       */
+/*   Updated: 2020/09/29 19:14:42 by pako             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,7 @@ t_flags	ft_putptrmaster2(unsigned long n, t_flags data)
 	{
 		if ((data.isprecition == 1) && (data.width > 0))
 		{
-			if (data.minus == 0 && data.width > 1)
+			if (data.minus == 0 && data.width > 2)
 				data.ret += write(1, " ", 1);
 			data.ret += write(1, "0x", 2);
 			if ((data.minus == 1) && (data.width > 2))
