4619dc9e25ebc1dbada45af44d4f65d42e1b253d
PakoPortalo <pakoportalo@gmail.com>
2020-09-30 16:12:14 +0200

    printflover 2

--- ft_putptrmaster.c
+++ ft_putptrmaster.c
@@ -6,7 +6,7 @@
 /*   By: pako <pako@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/24 19:29:14 by pako              #+#    #+#             */
-/*   Updated: 2020/09/30 16:07:54 by pako             ###   ########.fr       */
+/*   Updated: 2020/09/30 16:09:24 by pako             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 
 t_flags	ft_ptrprinter(t_flags data)
 {
-	if (((data.isprecition == 0) || (data.precition == 0)) && (data.width >= 2))
-		data.width -= 2; //sdfgsfgsdfgsdgsdfgsdfgsdfgsdfg
 	if ((data.minus == 1) && (data.nbr < 0))
 		data.width++;
 	if ((data.width > data.digit) && (data.zero == 0))
@@ -82,10 +80,7 @@ t_flags	ft_putptrmaster(unsigned long n, t_flags data)
 	{
 		if ((data.precition > data.digit) || (data.width > data.precition) || \
 		((data.precition == 0) && (data.width == 0)))
-		{
 			data.ret += write(1, "0x", 2);
-			data.width -= 2;  //asdfasdfas
-		}
 		data = ft_putptr(n, data);
 	}
 	if (data.minus == 1)
